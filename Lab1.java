fb68e841612d77f125fbeb84767dd1a658c92b29
Chung Ting Lin <66563386+tim1207@users.noreply.github.com>
2020-06-09 09:11:32 +0800

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -6,37 +6,41 @@ public class Lab1 {
 	public static void main(String[] args) {
 		int max=99;
 		int min=0;
-		// generate random integers¥Í²£¶Ã¼Æ
+		
 		int x = (int) (Math.random()*100);
 		System.out.println("0~99");
 		while (true) {
 			// user input
 			if(max == min) {
 				System.out.println("Game over");
-				break;}
+				break;
+			}
 			Scanner input=new Scanner(System.in);
 			System.out.println("your answer:");
 			int y=input.nextInt();
-		if(y==x) {
-			System.out.println("correct");
+			if(y==x) {
+				System.out.println("correct");
 			}
-		else if(y>x&&y<99) {
-			max=y-1;
+			else if(y>x&&y<99) {
+				max=y-1;
 			if(max == min) {
 				System.out.println("Game over");
-				break;}
-			System.out.println("Too large"+(min)+"~"+(max));
+				break;
 			}
-		else if(y<x&&y>0){
-			min=y+1;
+				System.out.println("Too large"+(min)+"~"+(max));
+			}
+			else if(y<x&&y>0){
+				min=y+1;
 			
 			if(max == min) {
 				System.out.println("Game over");
-				break;}
+				break;
+			}
 			System.out.println("Too small"+(min)+"~"+(max));
 			}
-		else if(y>max||y<min) {	
-			System.out.println("Out of range");
+			else if(y>max||y<min) 
+			{	
+				System.out.println("Out of range");
 			}	
 		
 		
